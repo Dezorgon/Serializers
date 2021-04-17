2dd4e3adcf4e4eae70ed7078c2f53b282f46f992
Dezorgon <superemail003@gmail.com>
2021-04-17 15:43:51 +0300

    'setup'

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
-from setuptools import find_packages
 from setuptools import setup
 
 setup(
     name='wtf_converter',
     version='1.0.0',
-    packeges=find_packages(exclude=('tests*',))
+    author="Dezorgon",
+    python_requires=">=3.6",
+    packages=['parsers.json_parser', 'parsers.toml_parser',
+              'parsers.yaml_parser', 'parsers.pickle_parser',
+              'serializer_creator']
 )
